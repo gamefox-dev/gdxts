b660444904f6420fa06569f4c25095f1dfed789e
Richtervn <tuanvuflp@gmail.com>
2022-07-03 09:14:32 +0700

    vector2

---
+++ src/lib/Vector2.ts
@@ -0,0 +1,87 @@
+export default class Vector2 {
+  x = 0;
+  y = 0;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  getX(): number {
+    return this.x;
+  }
+  getY(): number {
+    return this.y;
+  }
+  set(x: number, y: number): Vector2 {
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+  setVector(v: Vector2): Vector2 {
+    return this.set(v.x, v.y);
+  }
+  add(x: number, y: number): Vector2 {
+    this.x += x;
+    this.y += y;
+    return this;
+  }
+  addVector(v: Vector2): Vector2 {
+    return this.add(v.x, v.y);
+  }
+  sub(x: number, y: number): Vector2 {
+    return this.add(-x, -y);
+  }
+  subVector(v: Vector2): Vector2 {
+    return this.sub(v.x, v.y);
+  }
+  scale(scalar: number): Vector2 {
+    this.x *= scalar;
+    this.y *= scalar;
+    return this;
+  }
+  dot(v: Vector2): number {
+    return this.x * v.x + this.y * v.y;
+  }
+  distanceSqr(v: Vector2): number {
+    const deltaX = this.x - v.x;
+    const deltaY = this.y - v.y;
+    return deltaX * deltaX + deltaY * deltaY;
+  }
+  distance(v: Vector2): number {
+    return Math.sqrt(this.distanceSqr(v));
+  }
+  angle(): number {
+    return Math.atan2(this.y, this.x);
+  }
+  len2(): number {
+    return this.x * this.x + this.y * this.y;
+  }
+  len(): number {
+    return Math.sqrt(this.len2());
+  }
+  nor(): Vector2 {
+    const len = this.len();
+    if (len !== 0) {
+      this.x /= len;
+      this.y /= len;
+    }
+    return this;
+  }
+  rotateRad(rad: number): Vector2 {
+    const cos = Math.cos(rad);
+    const sin = Math.sin(rad);
+
+    const newX = this.x * cos - this.y * sin;
+    const newY = this.x * sin + this.y * cos;
+
+    this.x = newX;
+    this.y = newY;
+
+    return this;
+  }
+  rotate(degrees: number): Vector2 {
+    return this.rotateRad((degrees * Math.PI) / 180);
+  }
+  cpy() {
+    return new Vector2(this.x, this.y);
+  }
+}
