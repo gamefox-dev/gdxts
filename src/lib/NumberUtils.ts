680e55c81743ba8e9f6ff994297a3cb17d5fdb80
Richtervn <tuanvuflp@gmail.com>
2022-07-03 10:49:32 +0700

    number utils

---
+++ src/lib/NumberUtils.ts
@@ -0,0 +1,33 @@
+var int8 = new Int8Array(4);
+var int32 = new Int32Array(int8.buffer, 0, 1);
+var float32 = new Float32Array(int8.buffer, 0, 1);
+
+export class NumberUtil {
+  static intBitsToFloat(i: number): number {
+    int32[0] = i;
+    return float32[0];
+  }
+  static floatToIntBits(f: number): number {
+    float32[0] = f;
+    return int32[0];
+  }
+  static intToFloatColor(value: number): number {
+    return NumberUtil.intBitsToFloat(value & 0xfeffffff);
+  }
+  static colorToFloat(r: number, g: number, b: number, a: number): number {
+    var bits = (a << 24) | (b << 16) | (g << 8) | r;
+    return NumberUtil.intToFloatColor(bits);
+  }
+  static isPowerOfTwo(n: number): boolean {
+    return (n & (n - 1)) === 0;
+  }
+  static nextPowerOfTwo(n: number): number {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    return n + 1;
+  }
+}
