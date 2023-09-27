b0f474e0287f6c7974e39b4d8b4b06519017f197
implicit-invocation <tao@dongbat.com>
2023-09-28 01:36:25 +0700

    unpublished padding for splitTextures

--- src/lib/TextureRegion.ts
+++ src/lib/TextureRegion.ts
@@ -3,7 +3,22 @@ import { PolygonBatch } from './PolygonBatcher';
 import { Texture } from './Texture';
 
 export class TextureRegion {
-  static splitTexture(texture: Texture, cols: number, rows: number): TextureRegion[] {
+  static splitTexture(
+    texture: Texture,
+    cols: number,
+    rows: number,
+    padding?: number | [number, number] | [number, number, number, number]
+  ): TextureRegion[] {
+    if (padding === undefined) {
+      padding = 0;
+    }
+    if (typeof padding === 'number') {
+      padding = [padding, padding, padding, padding];
+    }
+    if (padding.length === 2) {
+      padding = [padding[0], 0, padding[1], 0];
+    }
+
     const { width, height } = texture;
     const regionWidth = width / cols;
     const regionHeight = height / rows;
@@ -12,7 +27,16 @@ export class TextureRegion {
 
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < cols; x++) {
-        regions.push(new TextureRegion(texture, x * regionWidth, y * regionHeight, regionWidth, regionHeight, {}));
+        regions.push(
+          new TextureRegion(
+            texture,
+            x * regionWidth + padding[0],
+            y * regionHeight + padding[2],
+            regionWidth - padding[0] - padding[1],
+            regionHeight - padding[2] - padding[3],
+            {}
+          )
+        );
       }
     }
     return regions;
